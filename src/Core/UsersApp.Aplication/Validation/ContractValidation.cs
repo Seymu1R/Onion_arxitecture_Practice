fc7c8ca8b02da9fbd6b0f895946428a516536543
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-10-06 23:50:02 +0400

    Done

---
+++ src/Core/UsersApp.Aplication/Validation/ContractValidation.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersApp.Domain.Entities;
+
+namespace UsersApp.Aplication.Validation
+{
+    public class ContractValidation : AbstractValidator<ConTract>
+    {
+        public ContractValidation()
+        {
+            RuleFor(c => c.Title).NotNull().NotEmpty();
+            RuleFor(c => c.Description).NotNull().NotEmpty();
+            RuleFor(c => c.UserId).NotEmpty().NotNull();
+
+        }
+
+        
+    }
+}
---
+++ src/Core/UsersApp.Aplication/Validation/UserValidation.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersApp.Domain.Entities;
+
+namespace UsersApp.Aplication.Validation
+{
+    public class UserValidation:AbstractValidator<User>
+    {
+        public UserValidation()
+        {
+            RuleFor(u=>u.Name).NotNull().NotEmpty();
+            RuleFor(u => u.UserName).NotNull().NotEmpty();
+            RuleFor(u => u.SurName).NotNull().NotEmpty();
+            RuleFor(u => u.FatherName).NotNull().NotEmpty();
+            RuleFor(u => u.Age).GreaterThan(0);
+        }
+
+    }
+}
